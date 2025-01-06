3565774085562b3c55a0d00460028e94d641ff68
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2025-01-06 07:14:31 -0800

    add invoice to db 2

---
+++ Task management/Areas/Admin/APIsControllers/InvoiceHeaderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task_management.Areas.Admin.APIsControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceHeaderController : ControllerBase
+    {
+        IIInvoseHeder iInvoseHeder;
+        UserManager<ApplicationUser> userManager;
+        public InvoiceHeaderController(IIInvoseHeder iInvoseHeder, UserManager<ApplicationUser> userManager)
+        {
+            this.iInvoseHeder = iInvoseHeder;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddHeaderForInvoice(TBInvoiceHederHelper model)
+        {
+            var user = await userManager.GetUserAsync(User);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var invoiceHeader = new TBInvoseHeder
+            {
+                 IdCustomerCategorie = model.IdCustomerCategorie,
+                 InvoiceNumber = model.InvoiceNumber,
+                 IdPaymentMethod = model.IdPaymentMethod,
+                 DataEntry = user.UserName,
+                 DateInvos = DateTime.UtcNow,
+                 DateTimeEntry = DateTime.UtcNow,
+                 IdUser = user.Id,
+                 OutstandingBill = true,
+                 CurrentState = true,
+            };
+
+            iInvoseHeder.saveData(invoiceHeader);
+            return Ok(invoiceHeader); 
+        }
+    }
+}
