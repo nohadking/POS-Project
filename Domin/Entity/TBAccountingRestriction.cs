0239a58db8064cd394895d885d35258294355476
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2025-01-29 03:52:56 -0800

    ...

--- Domin/Entity/TBAccountingRestriction.cs
+++ Domin/Entity/TBAccountingRestriction.cs
@@ -11,18 +11,18 @@ namespace Domin.Entity
     {
         [Key]
         public int IdaccountingRestrictions { get; set; }
-        public int NumberaccountingRestrictions { get; set; }
-        public string AccountingName { get; set; }
-        public string BondType { get; set; }
-        public decimal Debtor { get; set; }
-        public decimal creditor { get; set; }
+        public int NumberaccountingRestrictions { get; set; } // max number + 1 
+        public string AccountingName { get; set; }  // supllier
+        public string BondType { get; set; } // سند شراء 
+        public decimal Debtor { get; set; } // 0
+        public decimal creditor { get; set; } // القيمة الاجمالية للسند total price for purcheas
         [Required(ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "VlStatementPurchase")]
         [MaxLength(500, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength500")]
         [MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
-        public string Statement { get; set; }
+        public string Statement { get; set; } // 
         [MaxLength(500, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MaxLength500")]
         [MinLength(3, ErrorMessageResourceType = typeof(Resource.ResourceData), ErrorMessageResourceName = "MinLength3")]
-        public string? Nouts { get; set; }
+        public string? Nouts { get; set; } // 
         public string DataEntry { get; set; }
         public DateTime DateTimeEntry { get; set; }
         public bool CurrentState { get; set; }
