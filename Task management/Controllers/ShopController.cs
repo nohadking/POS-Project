3f0be80ca9fda638e929bf2872e5f096d37d1fbc
nohad darweish <nohadking@hotmail.com>
2025-02-10 17:22:53 +0300

    update all

--- Task management/Controllers/ShopController.cs
+++ Task management/Controllers/ShopController.cs
@@ -29,31 +29,71 @@ namespace Task_management.Controllers
 
 
         }
-        public IActionResult MyShop()
-		{
-			ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
-			vmodel.ListViewPhotoHomeSliderContent = iPhotoHomeSliderContent.GetAll();
-			vmodel.ListHomeSliderContent = iHomeSliderContent.GetAll();
-			vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
+        public IActionResult MyShop(int page = 1, List<string> selectedCategories = null)
+        {
+            int pageSize = 12;
+
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            vmodel.ListViewPhotoHomeSliderContent = iPhotoHomeSliderContent.GetAll();
+            vmodel.ListHomeSliderContent = iHomeSliderContent.GetAll();
+            vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
             vmodel.ListAboutSectionStartShopContent = iAboutSectionStartShopContent.GetAll().Take(1).ToList();
             vmodel.ListCategory = iCategory.GetAll();
 
+            // تصفية المنتجات بناءً على الفئة إذا تم اختيارها
+            var products = iProduct.GetAll();
+            if (selectedCategories != null && selectedCategories.Any())
+            {
+                products = products.Where(p => selectedCategories.Contains(p.CategoryNameEn)).ToList();
+            }
+
+            // استخدام Pagination للمنتجات
+            vmodel.ListViewProduct = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // حساب عدد الصفحات
+            var totalProducts = products.Count();
+            ViewBag.TotalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+            ViewBag.CurrentPage = page;
 
             return View(vmodel);
-		}   
-		public IActionResult MyShopAr()
+        }
+
+
+        public IActionResult MyShopAr(int page = 1, List<string> selectedCategories = null)
 		{
-			ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
-			vmodel.ListViewPhotoHomeSliderContent = iPhotoHomeSliderContent.GetAll();
-			vmodel.ListHomeSliderContent = iHomeSliderContent.GetAll();
-			vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
+            int pageSize = 12;
+
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            vmodel.ListViewPhotoHomeSliderContent = iPhotoHomeSliderContent.GetAll();
+            vmodel.ListHomeSliderContent = iHomeSliderContent.GetAll();
+            vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
-			vmodel.ListAboutSectionStartShopContent = iAboutSectionStartShopContent.GetAll().Take(1).ToList();
+            vmodel.ListAboutSectionStartShopContent = iAboutSectionStartShopContent.GetAll().Take(1).ToList();
             vmodel.ListCategory = iCategory.GetAll();
 
+            // تصفية المنتجات بناءً على الفئة إذا تم اختيارها
+            var products = iProduct.GetAll();
+            if (selectedCategories != null && selectedCategories.Any())
+            {
+                products = products.Where(p => selectedCategories.Contains(p.CategoryNameAr)).ToList();
+            }
+
+            // استخدام Pagination للمنتجات
+            vmodel.ListViewProduct = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // حساب عدد الصفحات
+            var totalProducts = products.Count();
+            ViewBag.TotalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+            ViewBag.CurrentPage = page;
 
             return View(vmodel);
-		}
+        }
 	}
 }
