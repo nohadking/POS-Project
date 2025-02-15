c16939046c69cf743a73dd9fcdcf1b7e41ae67fa
nohad darweish <nohadking@hotmail.com>
2025-02-15 11:39:03 +0300

    install checkoute controuler + view by nohad in 15-2-2024

--- Task management/Controllers/CheckOutController.cs
+++ Task management/Controllers/CheckOutController.cs
@@ -1,32 +1,31 @@
-﻿using Infarstuructre.BL;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Task_management.Controllers
 {
-    public class CartController : Controller
+    [Authorize]
+    public class CheckOutController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        IICompanyInformation iCompanyInformation;            
-        IIHomeBackgroundimage iHomeBackgroundimage;    
-    
-        public CartController(ILogger<HomeController> logger, IICompanyInformation iCompanyInformation1, IIHomeBackgroundimage iHomeBackgroundimage1)
+        IICompanyInformation iCompanyInformation;
+        IIHomeBackgroundimage iHomeBackgroundimage;
+        public CheckOutController(ILogger<HomeController> logger, IICompanyInformation iCompanyInformation1, IIHomeBackgroundimage iHomeBackgroundimage1)
         {
             _logger = logger;
-            iCompanyInformation = iCompanyInformation1;            
-            iHomeBackgroundimage = iHomeBackgroundimage1;             
+            iCompanyInformation = iCompanyInformation1;
+            iHomeBackgroundimage = iHomeBackgroundimage1;
         }
-        public IActionResult MyCart()
+        public IActionResult MYCheckOut()
         {
             ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
             vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
             return View(vmodel);
-        }    public IActionResult MyCartAr()
+        }
+        public IActionResult MYCheckOutAr()
         {
             ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
             vmodel.ListCompanyInformation = iCompanyInformation.GetAll();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
-          
             return View(vmodel);
         }
     }
