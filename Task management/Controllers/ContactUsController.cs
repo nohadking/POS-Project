2f7583d5193d18f24f50cb6a9ab6c444de7205ec
nohad darweish <nohadking@hotmail.com>
2025-02-15 15:43:42 +0300

    update Abut us and contact us by nohad

--- Task management/Controllers/ContactUsController.cs
+++ Task management/Controllers/ContactUsController.cs
@@ -1,33 +1,30 @@
-﻿using Infarstuructre.BL;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Task_management.Controllers
 {
-    public class CartController : Controller
+    public class ContactUsController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        IICompanyInformation iCompanyInformation;            
-        IIHomeBackgroundimage iHomeBackgroundimage;    
-    
-        public CartController(ILogger<HomeController> logger, IICompanyInformation iCompanyInformation1, IIHomeBackgroundimage iHomeBackgroundimage1)
+        IICompanyInformation iCompanyInformation;
+        IIHomeBackgroundimage iHomeBackgroundimage;
+        public ContactUsController(ILogger<HomeController> logger, IICompanyInformation iCompanyInformation1, IIHomeBackgroundimage iHomeBackgroundimage1)
         {
             _logger = logger;
-            iCompanyInformation = iCompanyInformation1;            
-            iHomeBackgroundimage = iHomeBackgroundimage1;             
+            iCompanyInformation = iCompanyInformation1;
+            iHomeBackgroundimage = iHomeBackgroundimage1;
         }
-        public IActionResult MyCart()
+        public IActionResult ContactUs()
         {
             ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
             vmodel.ListCompanyInformation = iCompanyInformation.GetAll().Take(1).ToList();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
             return View(vmodel);
         }  
-        public IActionResult MyCartAr()
+        public IActionResult ContactUsAr()
         {
             ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
             vmodel.ListCompanyInformation = iCompanyInformation.GetAll().Take(1).ToList();
             vmodel.ListHomeBackgroundimage = iHomeBackgroundimage.GetAll().Take(1).ToList();
-          
             return View(vmodel);
         }
     }
